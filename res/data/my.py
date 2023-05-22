319845c93607a6c5c66a9de5dbc1d94dc85ed015
CodingRookie <520whjoker@gmail.com>
2023-05-22 14:11:12 +0800

    fix 备忘单代码bug 和代码逻辑

--- res/data/my.py
+++ res/data/my.py
@@ -1,13 +1,14 @@
 import json
 
 # 读取 TopicInfo.json 文件
-with open('TopicInfo.json', 'r', encoding="utf-8") as file:
+with open('cheatSheet_new.json', 'r', encoding="utf-8") as file:
     data = json.load(file)
 
 # 遍历每个项并添加 "group" 值
-for item in data["topicInfoArray"]:
-    item["group"] = "A"
+for item in data["quantifiersAndAlternation"]:
+    item["text"] = ""
+    item["pattern"] = ""
 
 # 将更新后的数据写入新文件 TopicInfo_new.json
-with open('TopicInfo_new.json', 'w', encoding="utf-8") as file:
+with open('cheatSheet_new.json', 'w', encoding="utf-8") as file:
     json.dump(data, file, ensure_ascii=False)
